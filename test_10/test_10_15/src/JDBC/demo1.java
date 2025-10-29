43d21c94836fb52758c54273738b391f529cd820
mulaihongliqi627-lgtm <mulaihongliqi627@gmail.com>
2025-10-29 23:04:23 +0800

    jdbc实现数据库的增删查改

--- test_10/test_10_15/src/JDBC/demo1.java
+++ test_10/test_10_15/src/JDBC/demo1.java
@@ -1,4 +1,4 @@
-package jdbc;
+package JDBC;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
@@ -8,36 +8,34 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-//通过jdbc实现对数据库的增删改操作
+//数据库增删改
 public class demo1 {
     public static void main(String[] args) throws SQLException {
-        Scanner sc = new Scanner(System.in);
-        //1.建立数据源
+        //1.创建数据源
         DataSource dataSource = new MysqlDataSource();
         ((MysqlDataSource)dataSource).setUrl("jdbc:mysql://127.0.0.1:3306/java?characterEncoding=utf8&useSSL=false");
         ((MysqlDataSource)dataSource).setUser("root");
         ((MysqlDataSource)dataSource).setPassword("Lzj051029.");
 
-        //2.和数据源建立连接
-        Connection connection = dataSource.getConnection();
-
-        //3.编写sql语句,java构造sql字符串,转化为服务器可执行的sql语句statement交给服务器执行
+        //2.和数据库建立连接
+        Connection connection =  dataSource.getConnection();
+        //3.创建sql语句
+        Scanner sc = new Scanner(System.in);
         System.out.println("请输入学号");
         int id = sc.nextInt();
         System.out.println("请输入姓名");
         String name = sc.next();
         String sql = "insert into student values(?,?)";
+        //4.客户端预处理语句,把sql字符串转化为服务器可识别的statement语句
         PreparedStatement statement = connection.prepareStatement(sql);
-
         statement.setInt(1,id);
         statement.setString(2,name);
-
-        //4.服务器执行sql语句
-         int n = statement.executeUpdate();//n代表sql语句影响的行数
-        System.out.println("n = " + n);
-
-        //5.释放资源,和创建顺序相反
+        //5.把语句发给服务器去执行
+        int n = statement.executeUpdate();
+        System.out.println("n = " + n);//n代表上诉sql语句影响了多少行
+        //6.释放资源,释放顺序和创建顺序相反
         statement.close();
         connection.close();
+
     }
 }
--- test_10/test_10_15/src/JDBC/demo2.java
+++ test_10/test_10_15/src/JDBC/demo2.java
@@ -1,4 +1,4 @@
-package jdbc;
+package JDBC;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
@@ -7,36 +7,32 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
 
-//通过jdbc实现对数据库的查询操作
+//数据库查询
 public class demo2 {
     public static void main(String[] args) throws SQLException {
-        Scanner sc = new Scanner(System.in);
-        //1.建立数据源
+        //1.创建数据源
         DataSource dataSource = new MysqlDataSource();
         ((MysqlDataSource)dataSource).setUrl("jdbc:mysql://127.0.0.1:3306/java?characterEncoding=utf8&useSSL=false");
         ((MysqlDataSource)dataSource).setUser("root");
         ((MysqlDataSource)dataSource).setPassword("Lzj051029.");
-
-        //2.和数据源建立连接
-        Connection connection = dataSource.getConnection();
-
-        //3.构造sql
+        //2.和数据库建立连接
+        Connection connection =  dataSource.getConnection();
+        //3.构造sql语句
         String sql = "select * from student";
+        //4.处理sql语句
         PreparedStatement statement = connection.prepareStatement(sql);
-
-        //4.sql语句执行成功后返回一个结果集
-        ResultSet resultSet = statement.executeQuery();
-
-        //5.遍历结果集
+        //5.把sql语句传给服务器执行
+        ResultSet resultSet = statement.executeQuery(sql);
+        //6.遍历结果集
         while (resultSet.next()){
-            int id = resultSet.getInt("id");
+//            int id= resultSet.getInt(1);
+//            String name = resultSet.getString(2);
+            int id= resultSet.getInt("id");
             String name = resultSet.getString("name");
             System.out.println(id + " " + name);
         }
-
-        //释放资源
+        //7.释放资源
         resultSet.close();
         statement.close();
         connection.close();
