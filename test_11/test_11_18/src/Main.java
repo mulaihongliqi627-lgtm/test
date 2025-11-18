97a27c9bdef28668f667beb6a433b0a3c588d3a5
mulaihongliqi627-lgtm <mulaihongliqi627@gmail.com>
2025-11-18 23:18:57 +0800

    code_practice

---
+++ test_11/test_11_18/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+     public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+     ListNode head = null;
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+
+    }
+}
